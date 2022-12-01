9ff625827be28da74f70123c31dc47cd2b492373
BjornSevenhuijsenPXL <73290553+BjornSevenhuijsenPXL@users.noreply.github.com>
2022-12-01 21:12:14 +0100

    Add files via upload

---
+++ PrepareFileForXRay.ps1
@@ -0,0 +1,23 @@
+﻿#Created by Björn Sevenhuijsen
+#----------------------------------------------
+#Change these file paths
+$inputLocation='C:\Users\mrmuc\Downloads\testng-results.xml'
+$outputLocation='C:\Users\mrmuc\Downloads\testResultsXRay.xml'
+
+$xml=[xml](Get-Content $inputLocation)
+
+$node=$xml.SelectSingleNode('//full-stacktrace')
+while ($node -ne $null)
+{
+    $node.ParentNode.RemoveChild($node)
+    $node = $xml.SelectSingleNode("//full-stacktrace")
+}
+
+$node=$xml.SelectSingleNode('//message')
+while ($node -ne $null)
+{
+    $node.ParentNode.RemoveChild($node)
+    $node = $xml.SelectSingleNode("//message")
+}
+
+$xml.Save($outputLocation)
